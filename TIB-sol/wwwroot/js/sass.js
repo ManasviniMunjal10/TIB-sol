5284d3f87e507e14db2fcd2c77e19d038f17ce3c
Manasvini Munjal <Manasvini@Falcon.local>
2021-02-11 08:53:09 -0800

    Add project files.

---
+++ TIB-sol/wwwroot/js/sass.js
@@ -0,0 +1,15 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**************************!*\
+  !*** ./Styles/site.scss ***!
+  \**************************/
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleError: Module Error (from ./node_modules/sass-loader/dist/cjs.js):\\nMissing binding C:\\\\Users\\\\manasvini\\\\source\\\\repos\\\\TIB-sol\\\\TIB-sol\\\\node_modules\\\\node-sass\\\\vendor\\\\win32-x64-72\\\\binding.node\\nNode Sass could not find a binding for your current environment: Windows 64-bit with Node.js 12.x\\n\\nFound bindings for the following environments:\\n  - Windows 64-bit with Node.js 14.x\\n\\nThis usually happens because your environment has changed since running `npm install`.\\nRun `npm rebuild node-sass` to download the binding for your current environment.\\n    at Object.emitError (C:\\\\Users\\\\manasvini\\\\source\\\\repos\\\\TIB-sol\\\\TIB-sol\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:448:6)\\n    at getSassImplementation (C:\\\\Users\\\\manasvini\\\\source\\\\repos\\\\TIB-sol\\\\TIB-sol\\\\node_modules\\\\sass-loader\\\\dist\\\\utils.js:55:21)\\n    at Object.loader (C:\\\\Users\\\\manasvini\\\\source\\\\repos\\\\TIB-sol\\\\TIB-sol\\\\node_modules\\\\sass-loader\\\\dist\\\\index.js:27:59)\");\n\n//# sourceURL=webpack://TIB-sol/./Styles/site.scss?");
+/******/ })()
+;
